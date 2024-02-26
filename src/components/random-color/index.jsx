53d6d32a7464e1f679c76e2e14bb91180736cb33
anyars-encarta <anyarsencarta@gmail.com>
2024-02-26 21:46:01 +0000

    Correct eslint errors

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -8,7 +8,7 @@ const RandomColor = () => {
   const randomColorUtility = (length) => Math.floor(Math.random() * length);
 
   const handleCreateRandomHexColor = () => {
-    // #678765 define hex 
+    // #678765 define hex
     const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     let hexColor = '#';
 
